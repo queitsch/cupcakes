abce7fd3a024890229ab57f4a8615f674d3072dc
queitsch <jannis.segebrecht@gmail.com>
2012-02-13 22:39:15 +0100

    added cuddlyness

--- hugtunnel.py
+++ hugtunnel.py
@@ -1,4 +1,14 @@
-#FIXME!
+#!/usr/bin/env pythonimport paramiko
+from contextlib import contextmanagerhost = '205.196.209.62'
+username = 'hugger'
+password = 'xoxoxo'@contextmanager
+
+#FIXME MORE!
 
 def hugtunnel(sender, receiver, hug_needz):
+    cuddlyrabbithole = paramiko.SSHClient()     #may need more testing
     pass
+
+#  (\_/)
+#  (^_^)
+# (")_(")
